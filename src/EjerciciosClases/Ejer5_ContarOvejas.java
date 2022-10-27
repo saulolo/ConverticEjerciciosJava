e7afdbba87dc3bd183b36f64b3b365a7961bc7e7
Saul Echeverri <saulolo@gmail.com>
2022-10-27 18:22:18 -0500

    Version 3. Ejercicio 5

---
+++ src/EjerciciosClases/Ejer5_ContarOvejas.java
@@ -0,0 +1,13 @@
+
+package EjerciciosClases;
+
+/*@author Saulolo*/
+public class Ejer5_ContarOvejas {
+    
+    public static void main(String[] args) {
+        
+        //subir
+        
+    }
+
+}
