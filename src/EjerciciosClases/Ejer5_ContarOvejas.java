63e3aa92ccd19e2a412217c655305cd0ba2f5d74
Saul Echeverri <saulolo@gmail.com>
2022-10-27 19:16:19 -0500

    Version 3. Ejercicio 5. Fecha: 27-05-22

--- src/EjerciciosClases/Ejer5_ContarOvejas.java
+++ src/EjerciciosClases/Ejer5_ContarOvejas.java
@@ -1,12 +1,37 @@
-
 package EjerciciosClases;
 
+import java.util.Scanner;
+
 /*@author Saulolo*/
+
 public class Ejer5_ContarOvejas {
     
     public static void main(String[] args) {
         
-        //subiral repo por neatbeans
+        Scanner input = new Scanner(System.in);
+        
+        /*//Se requiere construir un programa que cuente las ovejas que está 
+        soñando una persona.*/
+        
+        
+        
+        System.out.println("CONTANDO OVEJAS");
+        System.out.println("---------------");
+        
+        int contador = -1;
+        String answer;
+        
+        do {
+            System.out.println("Sigues dormido: ");
+            answer = input.next();
+
+                contador = contador + 1;
+                System.out.println("La cantidad de ovejas contadas es de: " + contador);
+        } while (answer.equals("si"));
+        
+        //Terminar en casa, corregir machetazo
+       
+       
         
     }
 
