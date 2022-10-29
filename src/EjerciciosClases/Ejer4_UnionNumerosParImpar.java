31c920c381d64b243f6cac402e83fa8d88f0fb09
Saul Echeverri <111350593+saulolo@users.noreply.github.com>
2022-10-29 00:30:08 -0500

    Version 3. Ejer4 union arreglos:Terminar 28-09-22

--- src/EjerciciosClases/Ejer4_UnionNumerosParImpar.java
+++ src/EjerciciosClases/Ejer4_UnionNumerosParImpar.java
@@ -19,46 +19,73 @@ public class Ejer4_UnionNumerosParImpar {
         System.out.println("UNIÓN DE NÚMEROS PARES E IMPARES");
         System.out.println("--------------------------------");
         
-        int diemnsionPares;
-        int diemnsionImpares;
+       
+        int j = 0;
+        int k = 0; 
+        int l = 0;
+       
         
-        //Arreglo Par
-        System.out.println("Ingrese la dimensión del arreglo Par: ");
-        diemnsionPares = input.nextInt();
-        
-        int pares[] = new int[diemnsionPares];
+        //Arreglo Par        
+        int pares[] = new int[6];
         
         System.out.println("Ingrese los números Pares a almacenar en el arreglo: ");
-        
-        for (int i = 0; i < diemnsionPares; i++) {
-            System.out.println((i + 1) + ". Ingrese el número Par: ");
+        //Rellenar el areglo Pares
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Ingrese el número Par " + (i + 1) + ":");
             pares[i] = input.nextInt();
         }
+        //Muestra los números almacebados en el areglo Pares
         System.out.println("los números Pares ingresados son: ");
-        for (int i = 0; i < diemnsionPares; i++) {
+        for (int i = 0; i < 6; i++) {
             System.out.print(pares[i] + " , ");   
         }
         System.out.println(" ");
-        System.out.println("===================================");
+        System.out.println("=================================================");
         System.out.println(" ");
         
          //Arreglo Impar
-        System.out.println("Ingrese la dimensión del arreglo Impar: ");
-        diemnsionImpares = input.nextInt();
-        
-        int impares[] = new int[diemnsionImpares];
+        int impares[] = new int[6];
         
         System.out.println("Ingrese los números Impares a almacenar en el arreglo: ");
-        
-        for (int j = 0; j < diemnsionImpares; j++) {
-            System.out.println((j + 1) + ". Ingrese el número Impar: ");
-            impares[j] = input.nextInt();
+        //Rellenar el areglo Impares
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Ingrese el número Par: " + (i + 1) + ":");
+            impares[i] = input.nextInt();
         }
+        
+        //Muestra los números almacebados en el areglo Impares
         System.out.println("los números Impares ingresados son: ");
-        for (int j = 0; j < diemnsionImpares; j++) {
-            System.out.print(impares[j] + " , ");
+        for (int i = 0; i < 6; i++) {
+            System.out.println(impares[i] + " , ");
+             }
+        
+        System.out.println(" ");
+        System.out.println("=================================================");
+        System.out.println(" ");    
+        
+        //Union de ambos vectores   
+        int sumaNumeros[] = new int[12];
+        
+        while (j < 6) { 
+            for(l=0;l<1;l++)
+            {
+                sumaNumeros[k]=pares[j+l];
+                k++;
+            }
+            for(l=0;l<1;l++)
+            {
+                sumaNumeros[k]=impares[j+l];
+                k++;
+            }
+            j=j+1;
             
-            //TERMINAR
+        }
+        System.out.println("La suma de los números pares e impares ingresados es: ");
+        for (k = 0; k < 12; k++) {
+            System.out.println(sumaNumeros[k]);
+            
+            //PULIR: - pedir dimension por usuario.
+            //       - Intercalar el orden del arreglo 
             
         }
 
