503d71a0cde6f7c0d694a7954c4bd5ba02f71cee
Saul Echeverri <saulolo@gmail.com>
2022-11-08 20:04:36 -0500

    Terminar el ejercicio de la clase  Fecha: 08-11-22

---
+++ src/EjerciciosClases/Ejer8_IdentificarNumero.java
@@ -0,0 +1,14 @@
+package EjerciciosClases;
+
+/*@author Saulolo*/
+
+public class Ejer8_IdentificarNumero {
+    
+    private Integer numero;
+    
+        /*Escribir un programa que permita identificar el primer y último dígito
+        de un número entero ingresado por el usuario.*/
+    
+    
+    }
+
--- src/EjerciciosClases/main.java
+++ src/EjerciciosClases/main.java
@@ -6,6 +6,12 @@ public class main {
     
     public static void main(String[] args) {
         
+        
+        Integer numero = 24;
+        
+        numero.toString();
+        //System.out.println(numero);
+        
 
     }
 }
